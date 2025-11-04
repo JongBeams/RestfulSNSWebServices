ad635ff1193664e973ab0a5bb4bd4905f2cdffcd
JongBeams <wwhow2003@gmail.com>
2025-11-04 14:37:06 +0900

    restful 사용 sns web services learning

---
+++ src/test/java/io/jongbeom/springboot/intellij/restfulwebsnsservices/RestfulWebSnsServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jongbeom.springboot.intellij.restfulwebsnsservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebSnsServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
